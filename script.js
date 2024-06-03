e5d00ee9a02b248afeb551efaa773ce8697252a8
s19835 <s19835@sci.pdn.ac.lk>
2024-06-03 00:51:03 -0700

    classes del, set width and height

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
 //setting draw style
 ctx.fillStyle = 'white';
 ctx.strokeStyle = 'white';
 
-class Particle {
-    constructor(effect) {
-        this.Effects = effect;
-        this.x = Math.floor(Math.random() * this.Effects.width);
-        this.y = Math.floor(Math.random() * this.Effects.height);
-    }
-    draw(context) {
-        context.fillRect(this.x, this.y, 10, 10);
-    }
-}
-
-class Effects {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        this.particles = [];
-    }
-
-    init() {
-        this.particles.push(new Particle(this));
-    }
-}
-
-const effect = new Effects(canvas.width, canvas.height);
-effect.init();
-const particle = new Particle(effect);
-particle.draw(ctx);
